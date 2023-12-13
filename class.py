6247422de1c3703198bfc73d794750fa7b09f8f2
pooja123-yadav <85959414+pooja123-yadav@users.noreply.github.com>
2023-12-14 01:18:32 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,23 @@
+
+class Dog:
+
+    # class attribute
+    attr1 = "mammal"
+    
+    # Instance attribute
+    # It is run as soon as an object of a class is instantiated. The method is useful to do any initialization you want to do with your object.
+    def __init__(self, name):
+    	self.name = name
+
+# Driver code
+# Object instantiation
+Rodger = Dog("Rodger")
+Tommy = Dog("Tommy")
+
+# Accessing class attributes
+print("Rodger is a {}".format(Rodger.__class__.attr1))
+print("Tommy is also a {}".format(Tommy.__class__.attr1))
+
+# Accessing instance attributes
+print("My name is {}".format(Rodger.name))
+print("My name is {}".format(Tommy.name))
